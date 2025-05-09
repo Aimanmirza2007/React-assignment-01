0f2189bd79d863f40fbf458f307e7b7571c12b90
Aimanmirza2007 <sm501623@gmail.com>
2025-05-09 15:44:38 +0500

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let arr = [
+    "box1",
+    "box2",
+    "box3",
+    "box4",
+    "box5",
+    "box6",
+    "box5",
+    "box6",
+    "box7",
+    "box8",
+    "box9",
+    "box10",
+  ];
+
+  return (
+    <>
+      <div className=" bg-teal-800 flex flex-wrap justify-center items-center gap-8 p-8">
+        {arr.map((item) => (
+          <div className="w-60 h-60 bg-white flex items-center justify-center  cursor-pointer hover:scale-105 duration-300 ease-in-out">
+            <div className="text-2xl font-bold ">{item}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
